8bd9a793711bdd9358101a935f67349f698a69ba
Ross Clark <ross.clark@and.digital>
2022-11-10 14:35:51 +0000

    initial commit with spring boot app

---
+++ src/main/java/com/andjemison/scheduland/SchedulandApplication.java
@@ -0,0 +1,13 @@
+package com.andjemison.scheduland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andjemison/scheduland/SchedulandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andjemison.scheduland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
