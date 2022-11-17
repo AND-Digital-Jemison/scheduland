c10c6229205f222097dfddb0963cae8826b93d59
Jack <jackrimmer2996@hotmail.co.uk>
2022-11-17 14:34:39 +0000

    Added terraform initial setup for elastic beanstalk

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+resource "aws_elastic_beanstalk_application" "schedulandtf" {
+  name = "scheduland"
+  description = "Scheduling App"
+}
+
+resource "aws_elastic_beanstalk_environment" "schedulandtfenv" {
+  name                = "schedulandtf"
+  application         = aws_elastic_beanstalk_application.schedulandtf.name
+  solution_stack_name = "64bit Amazon Linux 2 v3.4.1 running Corretto 17"
+  
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     =  "aws-elasticbeanstalk-ec2-role"
+  }
+}
