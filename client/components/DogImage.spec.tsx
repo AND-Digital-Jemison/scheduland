51b6db2db4988539a87a329c005a0a49d3bf033c
alice.broadhurst@and.digital <alice.broadhurst@and.digital>
2022-11-21 14:28:11 +0000

    added terraform buckets

---
+++ client/components/DogImage.spec.tsx
@@ -0,0 +1,7 @@
+import { render, screen, fireEvent } from "@testing-library/react"
+
+describe("DogImage", () => {
+    it("Should display a random image of a dog", () => {
+        
+    })
+})
--- terraform/main.tf
+++ terraform/main.tf
@@ -3,9 +3,18 @@ resource "aws_elastic_beanstalk_application" "schedulandtf" {
   description = "Scheduling App"
 }
 
-resource "aws_s3_bucket" "scheduland_code" {
-  value = '/../build'
+resource "aws_s3_bucket" "scheduland_code_bucket" {
+  bucket = "scheduland-prod"
+  acl = "private"
+
 }
+
+resource "aws_s3_bucket_object" "scheduland_code_bucket_object" {
+  bucket = aws_s3_bucket.scheduland_code_bucket.id
+  key = "beanstalk/scheduland"
+  source = "target/scheduland-1.0.0.jar"
+}
+
 
 resource "aws_elastic_beanstalk_environment" "schedulandtfenv" {
   name                = "schedulandtf"
