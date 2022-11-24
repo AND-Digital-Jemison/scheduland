1133603c387a12b9db2d86e1e8e5e84052031441
Elena Cavallero <elecav@hotmail.it>
2022-11-24 11:31:58 +0000

    delete required providers;

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
   }
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
+  # required_providers {
+  #   aws = {
+  #     source = "hashicorp/aws"
+  #   }
+  # }
 }
