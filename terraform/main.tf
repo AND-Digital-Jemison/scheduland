e41ab9e74928c0dfa0029202306e504cf1f3820a
Elena Cavallero <elecav@hotmail.it>
2022-11-25 13:35:44 +0000

    no version label

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,7 @@ resource "aws_elastic_beanstalk_application_version" "schedulandtf_version"{
 resource "aws_s3_bucket" "scheduland_code_bucket" {
   bucket = "scheduland-prod"
   acl = "private"
+
 }
 
 resource "aws_s3_bucket_object" "scheduland_code_bucket_object" {
@@ -26,7 +27,7 @@ resource "aws_elastic_beanstalk_environment" "schedulandtfenv" {
   name                = "schedulandtf"
   application         = aws_elastic_beanstalk_application.schedulandtf.name
   solution_stack_name = "64bit Amazon Linux 2 v3.4.1 running Corretto 17"
-  version_label = aws_elastic_beanstalk_application_version.schedulandtf_version.name
+  # version_label = aws_elastic_beanstalk_application_version.schedulandtf_version.name
 
   setting {
     name = "SERVER_PORT"
